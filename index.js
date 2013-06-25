52f49ffa6f31e3dc8ca54ecad5a217f692638ce4
Krilnon <krilnon@gmail.com>
2013-06-25 04:54:16 -0700

    Shuffle code around, remove non-Markdown productions.

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 var 
 	OMetaJS = require('ometa-js'),
 	OMeta = OMetaJS.core,
-	fs = require('fs')
+	Markdown,
+	fs = require('fs'),
+	util = require('util')
 
 var testString = '# Big Header\n\
 ## Sub Headen\n\
 ### Sub Sub Header\n\
-\n\
 This *is a* paragraph, _really_. You must\n\
 use **double** new lines to separate\n\
-the paragraphs\n\
-\n\
+the paragraphs\n\n\
 like this.\n'
 
 init()
 
 function init(){
-	fs.readFile(__dirname + '/' + 'markdown.ometa.js', { encoding: 'utf8' }, onGrammar)
+	var grammar = fs.readFileSync(__dirname + '/' + 'markdown.ometa.js', { encoding: 'utf8' })
+	var tree = OMetaJS.BSOMetaJSParser.matchAll(grammar, "topLevel", undefined, null /* error handler */)
+	Markdown = eval(OMetaJS.BSOMetaJSTranslator.match(tree, "trans"))
+
+	console.log(util.inspect(parse(testString), { depth: 5 }))
 }
 
-function onGrammar(err, data){
-	var tree = OMetaJS.BSOMetaJSParser.matchAll(data, "topLevel", undefined, null /* error handler */)
-	var res = eval(OMetaJS.BSOMetaJSTranslator.match(tree, "trans"))
-	console.log('test string: ', Markdown.matchAll(testString, 'Process'))
+function parse(str){
+	return Markdown.matchAll(str, 'Process')
+}
+
+// used within Markdown OMeta grammar
+function esc(s) {
+	return s.replace(/</g,"&lt").replace(/>/g,"&gt").replace(/\n/g,"<br/>")
+}
+
+// used within Markdown OMeta grammar
+function tag(tag, str) {
+	return { type: tag, value: str }
+}
+
+// used within Markdown OMeta grammar
+function objectString(input){
+	var arr = []
+	
+	var temp = []
+	for(var i = 0; i < input.length; i++){
+		var elem = input[i]
+		if(typeof elem == 'string'){
+			temp.push(input[i])
+		} else {
+			if(temp.length > 0){
+				arr.push(temp.join(''))
+				temp = []
+			}
+			
+			arr.push(elem)
+		}
+	}
+	
+	if(temp.length > 0) arr.push(temp.join(''))
+
+	return arr
+}
+
+
+
+module.exports = {
+	parse: parse
 }
--- markdown.ometa.js
+++ markdown.ometa.js
@@ -1,30 +1,19 @@
-function esc(s) {
-	return s.replace(/</g,"&lt").replace(/>/g,"&gt").replace(/\n/g,"<br/>");
-}
-function tag(tag, str) {
-	return " <"+tag+">"+str+"</"+tag+">";
-}
-
-ometa Markdown {
+ometa Markdown_ {
 	toEOL = (~seq('\n') anything)*:t '\n' -> t.join(""),
 	
 	//headers
 	h1 = "#"   ' ' toEOL:t -> tag("h1",t),
 	h2 = "##"  ' ' toEOL:t -> tag("h2",t),
 	h3 = "###" ' ' toEOL:t -> tag("h3",t),
-
 	
 	//paragraph
   paraend = '\n' ' '* '\n',
 	para =
-		(	latexdisplay
-    | latex
-    | expr
-		|	strong
+		(	strong
 		|	em
 		|	(~paraend anything)
 		)+:t
-		-> tag("p",t.join("")),
+		-> tag("p", objectString(t)),
       //	text = <(~seq('\n\n') anything)*>,
 	strong =
 		"**"
@@ -33,20 +22,11 @@ ometa Markdown {
 		-> tag("strong", t),
 	em =
 		( "*" <(~seq('*') anything)*>:t "*" 
-    | "_" <(~seq('_') anything)*>:t "_") 
+    	| "_" <(~seq('_') anything)*>:t "_") 
 		-> tag("em", t),
-	expr =
-		"{"
-		exp:t
-		"}"
-		-> tag("b", t),
-  latex = "$" (~seq('$') char)*:t "$"       
-          -> (" [beginmathjax]"+t.join("")+"[endmathjax]"),
-  latexdisplay = "$$" (~seq('$') char)*:t "$$" 
-                 -> (" [beginmathjaxdisplay]"+t.join("")+"[endmathjaxdisplay]"),
 
-  //blockquote
-                   blockquote = "> " para:t -> tag("blockquote",t),
+	//blockquote
+	blockquote = "> " para:t -> tag("blockquote",t),
 	
 	//code block
 	cbdelim =
@@ -57,23 +37,14 @@ ometa Markdown {
 		cbdelim
 		-> tag("pre", esc(t)),
 	
-	//inline expressions
-	num = <digit+>:n -> parseInt(n),
-	term = num,
-	expadd = term:a "+" term:b -> (a+b),
-	expmul = term:a "*" term:b -> (a*b),
-	expsub = term:a "-" term:b -> (a-b),
-	expdiv = term:a "/" term:b -> (a/b),
-	exp = (expmul|expdiv|expadd|expsub):e -> (" <b class='expr'>" + e + "</b>") ,
-	
 	//pull it all together
 	line = spaces (h3|h2|h1|blockquote|codeblock|para):t spaces -> t,
 	Process = line*
 };
 
-Markdown._enableTokens = function() {
+Markdown_._enableTokens = function() {
 	this.tokensEnabled = true;
 	OMeta._enableTokens.call(this, ['h1', 'h2', 'h3', 'strong', 'codeblock', 'expr', 'em','latex','latexdisplay']);
 }
 
-Markdown
+Markdown_
